d38a37383cad4973f4e7d07aae3b37da5c7a540a
jmyork <jen@yorkitconsulting.com>
2019-02-23 17:11:08 -0600

    Updates

---
+++ precache-manifest.9cfc3816e2715f0215db961f63bc1f31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "92291496055eec84470c",
+    "url": "/TicTacToe/static/js/runtime~main.92291496.js"
+  },
+  {
+    "revision": "cae89568cf5f405a881e",
+    "url": "/TicTacToe/static/js/main.cae89568.chunk.js"
+  },
+  {
+    "revision": "170d78dc7eaf86966fd3",
+    "url": "/TicTacToe/static/js/2.170d78dc.chunk.js"
+  },
+  {
+    "revision": "cae89568cf5f405a881e",
+    "url": "/TicTacToe/static/css/main.fc2bb90e.chunk.css"
+  },
+  {
+    "revision": "06c9a7f4c5cc6f55e536dae8eb527f06",
+    "url": "/TicTacToe/index.html"
+  }
+];
